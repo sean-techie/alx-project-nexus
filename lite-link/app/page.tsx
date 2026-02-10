86b569294eea09c81c870e8bca054516dabd81f1
sean-techie <reseanner@gmail.com>
2026-02-10 19:37:12 +0100

    feat: Structure and landing page completed

---
+++ lite-link/app/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+import Hero from "@/components/design/Hero";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col bg-zinc-50 font-sans dark:bg-black max-w-7xl mx-auto">
+      <Navbar />
+      <main className="flex w-full flex-col items-center justify-center py-8 px-8 bg-white dark:bg-black sm:items-start">
+        <Hero />
+      </main>
+      <Footer />
+    </div>
+  );
+}
